4d5e558bda8a2050fb2e0f248f74eafa11bdf4e0
Christian Estibar <cce7990@students.uc-bcf.edu.ph>
2023-10-03 12:08:33 +0800

    php-variables

---
+++ php_variable/integer.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Integer</title>
+</head>
+<body>
+    <?php
+
+       $int_var = 12345;
+       $another_int = -12345 + 12345;
+
+       echo "First = $int_var";
+       echo "<br>";
+       print "Second = $another_int";
+       
+    ?>
+</body>
+</html>
