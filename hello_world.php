aacc9795ab2a776b1b1cdb06d13a7b3e92e5c3fb
C-ESTIBAR5 <cce7990@students.uc-bcf.edu.ph>
2023-10-05 18:29:24 +0800

    lab act_push

--- hello_world.php
+++ hello_world.php
@@ -7,8 +7,16 @@
 </head>
 <body>
     <?php
-      echo "hello world";
+        $jon = "<b>JON</b>: Im a bastard of Winterfell";
+        
+        $tyrion = "<b>TYRION</b>: Never forget who you are, Wear it like armor,
+          and they can never use it against you";
+          
+          print "$jon<br>" ;  
+          print "$tyrion<br>" ;
 
+
+    
 ?>
 </body>
 </html>
